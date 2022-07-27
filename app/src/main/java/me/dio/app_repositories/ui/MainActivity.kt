528f89b5d667a3a11a2baf98a4b46b12726cc9b0
Luana <luana.mouro@gmail.com>
2022-07-27 14:38:49 -0300

    Commit 07

--- app/src/main/java/me/dio/app_repositories/ui/MainActivity.kt
+++ app/src/main/java/me/dio/app_repositories/ui/MainActivity.kt
@@ -11,13 +11,12 @@ import br.com.dio.app.repositories.core.createProgressDialog
 import br.com.dio.app.repositories.core.hideSoftKeyboard
 import br.com.dio.app.repositories.databinding.ActivityMainBinding
 import br.com.dio.app.repositories.presentation.MainViewModel
+import me.dio.app_repositories.databinding.ActivityMainBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
 
-    private val dialog by lazy { createProgressDialog() }
-    private val viewModel by viewModel<MainViewModel>()
-    private val adapter by lazy { RepoListAdapter() }
+
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,24 +24,23 @@ class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
-        binding.rvRepos.adapter = adapter
-
-        viewModel.repos.observe(this) {
-            when (it) {
-                MainViewModel.State.Loading -> dialog.show()
-                is MainViewModel.State.Error -> {
-                    createDialog {
-                        setMessage(it.error.message)
-                    }.show()
-                    dialog.dismiss()
-                }
-                is MainViewModel.State.Success -> {
-                    dialog.dismiss()
-                    adapter.submitList(it.list)
-                }
-            }
-        }
+        //binding.rvRepos.adapter = adapter
     }
+
+       // viewModel.repos.observe(this) {
+          //  when (it) {
+             //   MainViewModel.State.Loading -> dialog.show()
+             //   is MainViewModel.State.Error -> {
+              //      createDialog {
+                        setMessage(it.error.message)
+              //     }.show()
+               //     dialog.dismiss()
+                }
+               // is MainViewModel.State.Success -> {
+              //      dialog.dismiss()
+               //     adapter.submitList(it.list)
+              //  }
+          //  }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.main_menu, menu)
